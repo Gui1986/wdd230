75d54371f752e2f290895dd1fbbf992dbc53dd43
Marcos Guimarães <99098413+Gui1986@users.noreply.github.com>
2024-01-17 01:20:48 -0300

    Update

---
+++ scripts/dark-mode.js
@@ -0,0 +1,16 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("body");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000000";
+		main.style.color = "#3e8f40";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eeeeee";
+		main.style.color = "#000000";
+		modeButton.textContent = "🕶️";
+	}
+});
+
+
