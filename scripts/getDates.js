e3840db321c687e2d0ea8b84eafe791ac425bd87
Marcos Guimarães <99098413+Gui1986@users.noreply.github.com>
2024-01-16 21:01:05 -0300

    Delete scripts directory

--- scripts/getDates.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var lastModifiedTime = "2024-01-12";
-
-    document.getElementById('currentYear').textContent = new Date().getFullYear();
-
-    var currentDateTime = new Date();
-    document.getElementById('lastModified').textContent = 'Last Modified: ' + currentDateTime.toLocaleString('en');
-
-});
