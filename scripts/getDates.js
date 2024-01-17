a3571000658902e5761f20fde4b17dbe85625dbb
Marcos Guimarães <99098413+Gui1986@users.noreply.github.com>
2024-01-16 23:27:47 -0300

    update

---
+++ scripts/getDates.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let lastModifiedTime = "2024-01-12";
+
+    document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+    let currentDateTime = new Date();
+    document.getElementById('lastModified').textContent = 'Last Modified: ' + currentDateTime.toLocaleString('en');
+
+});
